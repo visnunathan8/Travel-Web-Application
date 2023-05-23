df2471062334fbc2b9e4af08b1daca05186f8673
jothibasulkv01 <59483225+jothibasulkv01@users.noreply.github.com>
2023-05-23 00:27:48 -0400

    Travel agent frontend services epic1

---
+++ FrontEnd/src/app/services/travelagent.service.spec.ts
@@ -0,0 +1,12 @@
+import { TestBed } from '@angular/core/testing';
+
+import { TravelagentService } from './travelagent.service';
+
+describe('TravelagentserviceService', () => {
+  beforeEach(() => TestBed.configureTestingModule({}));
+
+  it('should be created', () => {
+    const service: TravelagentService = TestBed.get(TravelagentService);
+    expect(service).toBeTruthy();
+  });
+});
---
+++ FrontEnd/src/app/services/travelagent.service.ts
@@ -0,0 +1,50 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { BehaviorSubject } from 'rxjs';
+
+export class travelagentaccount {
+  constructor(
+    public username: string,
+    public password: string,
+    public type: string,
+    public agentName ?: string,
+    public location ?: string
+  ){}
+}
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TravelagentService {
+
+  constructor(
+    private http:HttpClient
+  ) { }
+  loggedInStatus = false
+  setLoggedIn(value:boolean){
+    this.loggedInStatus = value
+  }
+  get isLoggedIn(){
+    return this.loggedInStatus
+  }
+
+  getTravelAgentAccount(username: string) {
+    return this.http.get<travelagentaccount>('http://localhost:8081/travelagent/findByUsername?username='+username);
+  }
+  checkTravelAgentAccount(travelagentaccount: travelagentaccount) {
+    return this.http.get<Number>('http://localhost:8081/travelagent/checkTravelAgent?username='+travelagentaccount.username+'&password='+travelagentaccount.password+'&accountType='+travelagentaccount.type);
+  }
+  createTravelAgentAccount(travelagentaccount: travelagentaccount) {
+       return this.http.post<travelagentaccount>('http://localhost:8081/travelagent/addTravelAgentAccount/', travelagentaccount);
+   }
+
+  updateTravelAgentAccount(travelagentaccount: travelagentaccount) {
+    return this.http.post<string>('http://localhost:8081/travelagent/updateTravelAgent/${id}', travelagentaccount);
+  }
+
+  getTravelAgentAccountList() {
+    return this.http.get<travelagentaccount[]>('http://localhost:8081/travelagent/findallTravelAgentAccounts/');
+  }
+}
+
