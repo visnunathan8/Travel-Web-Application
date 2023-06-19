2d5a8d860ad7651a5aae0a6f8447bb4de1dfdc81
jothibasulkv01 <59483225+jothibasulkv01@users.noreply.github.com>
2023-06-18 23:42:58 -0400

    Update travelagent.service.ts

--- FrontEnd/src/app/service/travelagent.service.ts
+++ FrontEnd/src/app/service/travelagent.service.ts
@@ -3,12 +3,14 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { BehaviorSubject } from 'rxjs';
 
 export class travelagentaccount {
+  editable!: boolean;
   constructor(
-    public username: string,
-    public password: string,
-    public type: string,
+    public username?: string,
+    public password?: string,
+    public type?: string,
     public agentName ?: string,
-    public location ?: string
+    public location ?: string,
+    public travelAgentId?: Number
   ){}
 }
 
@@ -29,22 +31,40 @@ export class TravelagentService {
     return this.loggedInStatus
   }
 
+  // getTravelAgentAccount(username: string) {
+  //   return this.http.get<travelagentaccount>('http://localhost:8081/travelagent/findbyUsername?username='+username);
+  // }
+  // checkTravelAgentAccount(travelagentaccount: travelagentaccount) {
+  //   return this.http.get<Number>('http://localhost:8081/travelagent/checkTravelAgent?username='+travelagentaccount.username+'&password='+travelagentaccount.password+'&accountType='+travelagentaccount.type);
+  // }
+  // createTravelAgentAccount(travelagentaccount: travelagentaccount) {
+  //      return this.http.post<travelagentaccount>('http://localhost:8081/travelagent/addTravelAgentAccount/', travelagentaccount);
+  //  }
+
+  // updateTravelAgentAccount(travelagentaccount: travelagentaccount) {
+  //   return this.http.post<string>('http://localhost:8081/travelagent/updateTravelAgent/', travelagentaccount);
+  // }
+
+  // getTravelAgentAccountList() {
+  //   return this.http.get<travelagentaccount[]>('http://localhost:8081/travelagent/findallTravelAgentAccounts/');
+  // }
+
   getTravelAgentAccount(username: string) {
-    return this.http.get<travelagentaccount>('http://localhost:8081/travelagent/findByUsername?username='+username);
+    return this.http.get<travelagentaccount>('https://floating-everglades-27882.herokuapp.com/travelagent/findbyUsername?username='+username);
   }
   checkTravelAgentAccount(travelagentaccount: travelagentaccount) {
-    return this.http.get<Number>('http://localhost:8081/travelagent/checkTravelAgent?username='+travelagentaccount.username+'&password='+travelagentaccount.password+'&accountType='+travelagentaccount.type);
+    return this.http.get<Number>('https://floating-everglades-27882.herokuapp.com/travelagent/checkTravelAgent?username='+travelagentaccount.username+'&password='+travelagentaccount.password+'&accountType='+travelagentaccount.type);
   }
   createTravelAgentAccount(travelagentaccount: travelagentaccount) {
-       return this.http.post<travelagentaccount>('http://localhost:8081/travelagent/addTravelAgentAccount/', travelagentaccount);
+       return this.http.post<travelagentaccount>('https://floating-everglades-27882.herokuapp.com/travelagent/addTravelAgentAccount/', travelagentaccount);
    }
 
   updateTravelAgentAccount(travelagentaccount: travelagentaccount) {
-    return this.http.post<string>('http://localhost:8081/travelagent/updateTravelAgent/${id}', travelagentaccount);
+    return this.http.post<string>('https://floating-everglades-27882.herokuapp.com/travelagent/updateTravelAgent/', travelagentaccount);
   }
 
   getTravelAgentAccountList() {
-    return this.http.get<travelagentaccount[]>('http://localhost:8081/travelagent/findallTravelAgentAccounts/');
+    return this.http.get<travelagentaccount[]>('https://floating-everglades-27882.herokuapp.com/travelagent/findallTravelAgentAccounts/');
   }
 }
 
