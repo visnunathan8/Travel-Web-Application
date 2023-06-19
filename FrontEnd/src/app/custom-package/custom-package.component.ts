303e4d23553809356d9c5e365e785fe19fad753f
SunithaGeorgee <SU_GEO@live.concordia.ca>
2023-06-18 22:49:22 -0400

    Modified custom package components

--- FrontEnd/src/app/custom-package/custom-package.component.ts
+++ FrontEnd/src/app/custom-package/custom-package.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivitiesService, Activity } from '../service/activities.service';
+import { customeraccount, customerAccountService } from '../service/customerAccount.service';
 import { Flight, FlightsService } from '../service/flights.service';
 import { Hotel, HotelService } from '../service/hotel.service';
 import { Travel, TravelpackageService } from '../service/travelpackage.service';
@@ -17,12 +18,22 @@ export class CustomPackageComponent implements OnInit {
   activitiesOptions: Activity[] = [];
   showAddForm: boolean = false;
   searchKeyword: string = '';
+  customer: customeraccount;
+  filteredTravels: Travel[] = [];
+  newcustomer: customeraccount = new customeraccount();
+  applySearchFilter() {
+    this.filteredTravels = this.travels.filter(
+      (travel) =>
+        travel.travelPackageName.toLowerCase().includes(this.searchKeyword.toLowerCase())
+    );
+  }
 
   constructor(
     private activitiesservice: ActivitiesService,
     private flightservice: FlightsService,
     private hotelservice: HotelService,
-    private travelService: TravelpackageService
+    private travelService: TravelpackageService,
+    private customerservice : customerAccountService,
   ) {}
 
   ngOnInit() {
@@ -30,6 +41,7 @@ export class CustomPackageComponent implements OnInit {
     this.getFlightOptions();
     this.getHotelOptions();
     this.getActivitiesOptions();
+    this.customer = history.state.customer;
   }
 
   newTravel: Travel = new Travel();
@@ -49,30 +61,51 @@ export class CustomPackageComponent implements OnInit {
     travel.flightId = Number(travel.flightId);
     travel.hotelId = Number(travel.hotelId);
     travel.isCustomerCreated = true;
-    this.travelService.createTravel(travel).subscribe(
-      (response) => {
-        console.log('Travel saved successfully');
-        this.resetForm();
-        this.showAddForm = false;
-        this.getTravelList();
-      },
-      (error) => {
-        console.log('Error occurred while saving travel:', error);
+    const customerState = JSON.parse(sessionStorage.getItem('customer'));
+    // const customerState = this.customerservice.getCustomerState();
+
+    this.customerservice.getCustomerAccount(customerState.username).subscribe(
+      (result: customeraccount) => {
+        this.newcustomer = result;
+        travel.customerId = Number(this.newcustomer.customerId);
+        this.travelService.createTravel(travel).subscribe(
+          (response) => {
+            console.log('Travel saved successfully');
+            this.resetForm();
+            this.showAddForm = false;
+            this.getTravelList();
+          },
+          (error) => {
+            console.log('Error occurred while saving travel:', error);
+          }
+        );
       }
-    );
+    )
   }
 
   getTravelList() {
-    this.travelService.getTravelList().subscribe(
-      (travels: Travel[]) => {
-        console.log(travels);
-        this.travels = travels.map((travel) => ({ ...travel, editable: false }));
-      },
-      (error) => {
-        console.log('Error occurred while retrieving travel list:', error);
+
+    // const customerState = this.customerservice.getCustomerState();
+    const customerState = JSON.parse(sessionStorage.getItem('customer'));
+
+    this.customerservice.getCustomerAccount(customerState.username).subscribe(
+      (result: customeraccount) => {
+        this.newcustomer = result;
+        this.travelService.getTravelListByCustomerID(Number(this.newcustomer.customerId)).subscribe(
+          (travels: Travel[]) => {
+            console.log(travels);
+            this.travels = travels.map((travel) => Object.assign({}, travel, { editable: false }));
+            this.filteredTravels = this.travels.map((travel) => Object.assign({}, travel));
+            this.applySearchFilter();
+          },
+          (error) => {
+            console.log('Error occurred while retrieving travel list:', error);
+          }
+        );
       }
-    );
+    )
   }
+
 
   toggleEdit(index: number) {
     if (this.travels[index].editable) {
